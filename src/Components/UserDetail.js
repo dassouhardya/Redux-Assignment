5f04df9d044d7700a598be85b1c9550fb3dd7d1a
Souhardya Das <66139595+dassouhardya@users.noreply.github.com>
2022-10-17 15:23:24 +0530

    Update UserDetail.js

--- src/Components/UserDetail.js
+++ src/Components/UserDetail.js
@@ -10,9 +10,11 @@ const Div = styled.div`
   margin: 25px;
   width: 100%;
 
-  @media only screen and (max-width: 800px) {
+    @media only screen and (max-width: 800px) {
     flex-direction: column;
     flex-wrap: nowrap;
+    flex: 100%;
+   
   }
 `;
 
